ef8f91341f4d249824c843753184c72ea0fda5e9
gue-ni <jakob.g.maier@gmail.com>
2024-12-26 18:16:52 +0100

    update

--- upload_videos.sh
+++ upload_videos.sh
@@ -3,16 +3,15 @@
 set -e
 set -x
 
-
 if pgrep -x "rsync" > /dev/null;
 then
 	echo "already running"
 	exit 1
 fi
 
-local_video_dir=/var/motion/
+local_dir=/var/motion/images/
 
 remote_server=root@www.jakobmaier.at
-remote_video_dir=/var/www/files/bird-pi/
+remote_dir=/var/www/files/birds-of-gartenweg/
 
-rsync --archive --verbose --delete $local_video_dir $remote_server:$remote_video_dir
+rsync --archive --verbose --delete $local_dir $remote_server:$remote_dir
